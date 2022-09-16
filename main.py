2b8a96bc931e48d3e651bf561d13b29b07fffa5f
s1ash <70847537+KvN1027@users.noreply.github.com>
2022-09-16 22:09:49 +0800

    nasa plz don't sent a rocket to my home

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+list = ["10%","25%","55%","70%","88%","99%","100%"]
+print("HACKING NASA TOOL")
+for i in range(7):
+    print("progress  ",list[i],end="... \n")
+    time.sleep(1)
+print("=====done=====")
+print("now NASA is yours:)")
